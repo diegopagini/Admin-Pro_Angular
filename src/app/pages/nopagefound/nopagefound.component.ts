da4357b62644329c681c735afc64a711eb6a86e3
Diego Pagini <diegopaginidev@gmail.com>
2021-01-14 17:00:11 -0300

    not found ready

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styleUrls: ['./nopagefound.component.css']
+  styleUrls: ['./nopagefound.component.css'],
 })
 export class NopagefoundComponent implements OnInit {
+  year: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  returnDashboard() {
+    this.router.navigateByUrl('/dashboard');
   }
-
 }
