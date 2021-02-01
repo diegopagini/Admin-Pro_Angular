d5d001bfc85e6896bdf9785c3e17c5af6d23a38c
Diego Pagini <dq@qbkconsulting.com>
2021-02-01 08:25:44 -0300

    agregando el check de manera general

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,14 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
+  public links;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
 
   changeTheme(theme: string) {
     const url = `./assets/css/colors/${theme}.css`;
     this.linkTheme.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach((element) => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    });
   }
 }
