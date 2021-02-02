5fcf234c6715f1f64569db22e54cb753fadd689d
Diego Pagini <dq@qbkconsulting.com>
2021-02-02 16:16:26 -0300

    breadcrums

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -13,12 +13,32 @@ const routes: Routes = [
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
-      { path: 'promesas', component: PromesasComponent },
-      { path: 'rxjs', component: RxjsComponent },
+      {
+        path: '',
+        component: DashboardComponent,
+        data: { titulo: 'Dashboard' },
+      },
+      {
+        path: 'progress',
+        component: ProgressComponent,
+        data: { titulo: 'Progress' },
+      },
+      {
+        path: 'grafica1',
+        component: Grafica1Component,
+        data: { titulo: 'Grafica1' },
+      },
+      {
+        path: 'account-settings',
+        component: AccountSettingsComponent,
+        data: { titulo: 'Account-Settings' },
+      },
+      {
+        path: 'promesas',
+        component: PromesasComponent,
+        data: { titulo: 'Promesas' },
+      },
+      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJS' } },
     ],
   },
 ];
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.css']
+  styleUrls: ['./breadcrumbs.component.css'],
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
+  public titulo: string;
+  public tituloSubs: Subscription;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {
+    console.log(this.route.snapshot.children[0].data);
 
-  ngOnInit(): void {
+    this.tituloSubs = this.getDataRuta().subscribe((data) => {
+      this.titulo = data.titulo;
+      document.title = `AdminPro - ${data.titutlo}`;
+    });
+  }
+  ngOnDestroy() {
+    this.tituloSubs.unsubscribe();
   }
 
+  getDataRuta() {
+    return this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
 }
