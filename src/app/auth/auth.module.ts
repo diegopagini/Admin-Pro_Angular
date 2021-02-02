be2072419c40f46ca6d2a87e091f7e896607beae
Diego Pagini <dq@qbkconsulting.com>
2021-02-02 07:37:27 -0300

    funcion global

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctinos();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -9,5 +11,7 @@ import { SettingsService } from '../services/settings.service';
 export class PagesComponent implements OnInit {
   constructor(private settingsService: SettingsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    customInitFunctinos();
+  }
 }
